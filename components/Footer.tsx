11624ba8e50a366b54f889041137f3d3d6b10f62
Raihan <raihanhn20@gmail.com>
2024-01-15 22:47:08 +0600

    footer completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-3">
+      This website is designed by GTCoding &copy; 2023
+    </footer>
+  );
 }

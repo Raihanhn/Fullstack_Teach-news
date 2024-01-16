a24beaba9a78d7cba30513286d916d8ee8a5d0de
Raihan <raihanhn20@gmail.com>
2024-01-16 20:50:29 +0600

    github logo added

--- components/SignInBtns.tsx
+++ components/SignInBtns.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import github from "../public/github-logo.svg";
 
 export default function SignInBtns() {
   return (
     <>
-      <h1 className="">Sign in</h1>
+      <h1 className="text-center mt-8 ">Sign in</h1>
+      <div className="">
+        <button>
+          <span>
+            <Image src={github} width={30} height={30} alt="GitHub Logo" />
+          </span>
+          Sign In with GitHub
+        </button>
+      </div>
     </>
   );
 }

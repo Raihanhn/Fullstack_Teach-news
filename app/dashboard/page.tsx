7a851afd655abf9bf905a3a6b78593a8f5254275
Raihan <raihanhn20@gmail.com>
2024-01-18 23:03:34 +0600

    Dashboard page created

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
-import CategoriesList from "@/components/CategoriesList";
-import Post from "@/components/Post";
+import React from "react";
 import { postsData } from "@/data";
+import Post from "@/components/Post";
+import Link from "next/link";
 
-export default function Home() {
+export default function Dashboard() {
   return (
-    <>
-      <CategoriesList />
+    <div>
+      <h1 className="">My Posts</h1>
+
       {postsData && postsData.length > 0 ? (
         postsData.map((post) => (
           <Post
@@ -22,8 +24,13 @@ export default function Home() {
           />
         ))
       ) : (
-        <div className="py-6">No posts to display </div>
+        <div className="py-6">
+          No posts created yet.{" "}
+          <Link className="underline" href={"/create-post"}>
+            Create New
+          </Link>
+        </div>
       )}
-    </>
+    </div>
   );
 }

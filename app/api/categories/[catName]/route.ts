0903addb97b73034ce605014e832cc0a984e2548
Raihan <raihanhn20@gmail.com>
2024-01-26 00:00:27 +0600

    category post route created

---
+++ app/api/categories/[catName]/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/app/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { catName: string } }
+) {
+  try {
+    const catName = params.catName;
+    const posts = await prisma.category.findUnique({
+      where: {
+        catName,
+      },
+      include: {
+        posts: { include: { author: true }, orderBy: { createdAt: "desc" } },
+      },
+    });
+    return NextResponse.json(posts);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Could not fetch post." });
+  }
+}

98bde44e1d81c4dd3bb855f3fca7beac725ec0d6
Raihan <raihanhn20@gmail.com>
2024-01-26 00:07:16 +0600

    author api route created

---
+++ app/api/authors/[email]/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/app/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { email: string } }
+) {
+  try {
+    const email = params.email;
+    const posts = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        posts: { orderBy: { createdAt: "desc" } },
+      },
+    });
+    return NextResponse.json(posts);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Could not fetch post." });
+  }
+}

c31bdd45954aeba881b21215ff15016a73d9db0e
Raihan <raihanhn20@gmail.com>
2024-01-25 23:39:29 +0600

    post delete method added

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -18,3 +18,51 @@ export async function GET(
     return NextResponse.json({ message: "Could not fetch post." });
   }
 }
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { title, content, links, selectedCategory, imageUrl, publicId } =
+    await req.json();
+  const id = params.id;
+
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+        links,
+        catName: selectedCategory,
+        imageUrl,
+        publicId,
+      },
+    });
+    return NextResponse.json(post);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error editing post" });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+
+  try {
+    const post = await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+    return NextResponse.json({ message: "Post deleted" });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error deleting post" });
+  }
+}

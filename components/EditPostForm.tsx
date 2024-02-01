7863bd6bc7deed7eb80492dd6c7f0b5423f169c8
Raihan <raihanhn20@gmail.com>
2024-02-01 21:55:49 +0600

    img can add or rmv from cloudinary

--- components/EditPostForm.tsx
+++ components/EditPostForm.tsx
@@ -3,6 +3,8 @@ import { TCategory, TPost } from "@/app/types";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { CldUploadButton, CldUploadWidgetResults } from "next-cloudinary";
+import Image from "next/image";
 
 export default function EditPostForm({ post }: { post: TPost }) {
   const [links, setLinks] = useState<string[]>([]);
@@ -46,6 +48,38 @@ export default function EditPostForm({ post }: { post: TPost }) {
 
   const deleteLink = (index: number) => {
     setLinks((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const handleImageUpload = (result: CldUploadWidgetResults) => {
+    console.log("result", result);
+    const info = result.info as object;
+    if ("secure_url" in info && "public_id" in info) {
+      const url = info.secure_url as string;
+      const public_id = info.public_id as string;
+      setImageUrl(url);
+      setPublicId(public_id);
+      console.log("url:", url);
+      console.log("public_id:", public_id);
+    }
+  };
+
+  const removeImage = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/removeImage", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ publicId }),
+      });
+
+      if (res.ok) {
+        setImageUrl("");
+        setPublicId("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const addLink = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -167,6 +201,48 @@ export default function EditPostForm({ post }: { post: TPost }) {
           </button>
         </div>
 
+        <CldUploadButton
+          uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
+          className={`h-48 border-2 mt-4 border-dotted grid place-items-center bg-slate-100 rounded-md relative ${
+            imageUrl && "pointer-events-none"
+          } `}
+          onUpload={handleImageUpload}
+        >
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+          {imageUrl && (
+            <Image
+              src={imageUrl}
+              fill
+              className="absolute object-cover inset-0 "
+              alt={title}
+            />
+          )}
+        </CldUploadButton>
+
+        {publicId && (
+          <button
+            onClick={removeImage}
+            className="py-2 px-4 rounded-md font-bold w-fit bg-red-600 text-white mb-4"
+          >
+            Remove Image
+          </button>
+        )}
+
         <select
           onChange={(e) => setSelectedCategory(e.target.value)}
           className="p-3 rounded-md border appearance-none"
@@ -181,7 +257,7 @@ export default function EditPostForm({ post }: { post: TPost }) {
             ))}
         </select>
         <button className="primary-btn" type="submit">
-          Create Post
+          Update Post
         </button>
         {error && <div className="p-2 text-red-500 font-bold">{error} </div>}
       </form>
